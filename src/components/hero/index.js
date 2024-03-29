2a59472ba2ca9050040a7dd9c716a92db8cc36d5
Ayabonga Qwabi <aya@simply.co.za>
2024-03-29 13:18:52 +0200

    add hero component

---
+++ src/components/hero/index.js
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+const Hero = ({ image, header, subHeader, text, cta }) => {
+  return (
+    <div
+      className={`container h-full bg-[url('/images/${image}')] h-full home-page-header flex flex-col justify-center items-center`}
+    >
+      <div className='grid md:grid-cols-2 grid-cols-1 flex flex-col justify-center items-center'>
+        <div className='h-full px-8 py-8 text-center md:text-left'>
+          <h1 className=' text-green-600 text-4xl mb-4 font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-900'>
+            {header}
+          </h1>
+          {subHeader && (
+            <h2 className=' text-white text-2xl mb-4 font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-900'>
+              {subHeader}
+            </h2>
+          )}
+          <p className=' text-white text-base/loose text-2xl md:text-2xl lg:text-2xl dark:text-gray-100 mb-6'>
+            {text}
+          </p>
+          <Link href={cta.link} className='text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  text-xl px-4 py-3 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+            {cta.title}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Layout from '@/components/layout';
+import Hero from '@/components/hero';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -9,20 +10,13 @@ export default function Home() {
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
       >
-        <h1 className='text-4xl font-bold text-center'>
-          Welcome to Music Loopy
-        </h1>
-        <Image
-          src='/logo.png'
-          alt='Music Loopy logo'
-          width={200}
-          height={200}
-          className='rounded-full'
+        <Hero
+          image='guitar.jpg'
+          header='Welcome to Music Loopy'
+          subHeader="Explore, Enjoy, Support"
+          text='Step into a thriving community of like-minded individuals who share your passion for music. From seasoned professionals to aspiring newcomers, Music Loopy welcomes all levels of talent and expertise. Connect with fellow creators, exchange ideas, and embark on exciting musical journeys together.'
+          cta={{ link: '', title: "Listen Now" }}
         />
-        <p className='text-center'>
-          Music Loopy is a platform for artists and producers to collaborate and
-          create music.
-        </p>
       </main>
     </Layout>
   );
