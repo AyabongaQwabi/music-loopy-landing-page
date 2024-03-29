a82d19344c4e99a753fa0762feb09ab29bb061a5
Ayabonga Qwabi <aya@simply.co.za>
2024-03-29 15:32:56 +0200

    make header large

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -7,20 +7,20 @@ const Hero = ({ image, header, subHeader, text, cta }) => {
     >
       <div className='grid md:grid-cols-2 grid-cols-1 flex flex-col justify-center items-center'>
         <div className='h-full px-8 py-8 text-center md:text-left'>
-          <h1 className=' text-green-600 text-4xl mb-4 font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-900'>
+          <h1 className=' drop-shadow-md text-green-600 text-7xl mb-4 font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-900'>
             {header}
           </h1>
           {subHeader && (
-            <h2 className=' text-white text-2xl mb-4 font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-900'>
+            <h2 className='drop-shadow-md my-4 text-white text-6xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-900'>
               {subHeader}
             </h2>
           )}
-          <p className=' text-white text-base/loose text-2xl md:text-2xl lg:text-2xl dark:text-gray-100 mb-6'>
-            {text}
-          </p>
-          <Link href={cta.link} className='text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  text-xl px-4 py-3 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+          
+          <div className='my-8'>
+          <Link href={cta.link} className='mt-4 text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  text-xl px-4 py-3 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
             {cta.title}
           </Link>
+          </div>
         </div>
       </div>
     </div>
